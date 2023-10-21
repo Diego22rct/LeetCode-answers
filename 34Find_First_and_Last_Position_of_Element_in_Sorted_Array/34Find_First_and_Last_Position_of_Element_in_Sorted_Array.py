ca8abff82206c5249d123b49e963f7266bd6355d
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2023-10-21 15:01:45 -0500

    add 34

---
+++ 34Find_First_and_Last_Position_of_Element_in_Sorted_Array/34Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        length = len(nums) - 1
+        return [
+            self.findLeft(nums, target, length),
+            self.findRight(nums, target, length),
+        ]
+
+    def findLeft(self, nums, target, length):
+        left, right = 0, length
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return left if nums[left] == target else -1
+
+    def findRight(self, nums, target, length):
+        left, right = 0, length
+        while left < right:
+            mid = (left + right + 1) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid
+        return left if nums[left] == target else -1
---
+++ 34Find_First_and_Last_Position_of_Element_in_Sorted_Array/34Find_First_and_Last_Position_of_Element_in_Sorted_Array1.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def binary_search(self, list, low, high, x):
+        if high >= low:
+            mid = (high + low) // 2
+            if list[mid] == x:
+                return mid
+            elif list[mid] > x:
+                return self.binary_search(list, low, mid - 1, x)
+            else:
+                return self.binary_search(list, mid + 1, high, x)
+        else:
+            return -1
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+
+        length = len(nums) - 1
+
+        return self.binary_search(
+            nums[: length // 2], 0, length, target
+        ), self.binary_search(nums[length // 2 :], 0, length, target)
